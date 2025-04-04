04acfe32dd52cbebdf682e15b78c52e154d1dffe
Nicolas <nicolascola21@gmail.com>
2025-04-04 21:36:24 +0200

    stili registrazione e login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
@@ -8,25 +8,29 @@ import { CommonModule } from '@angular/common';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [ReactiveFormsModule, CommonModule, RouterModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule
+  ],
   standalone: true
 })
 export class LoginComponent {
 
-  loginForm: UntypedFormGroup;
-  email = new UntypedFormControl('', [
+  loginForm: FormGroup;
+  email = new FormControl('', [
     Validators.email,
     Validators.required,
     Validators.minLength(3),
     Validators.maxLength(100)
   ]);
-  password = new UntypedFormControl('', [
+  password = new FormControl('', [
     Validators.required,
     Validators.minLength(6)
   ]);
 
   constructor(private auth: AuthService,
-              private formBuilder: UntypedFormBuilder,
+              private formBuilder: FormBuilder,
               private router: Router
             ) {
     this.loginForm = this.formBuilder.group({
