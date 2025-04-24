1efd1c8b832ec4434b8a6cb5dcf26d7dc77d172e
alessandrocampedelli <85106363+alessandrocampedelli@users.noreply.github.com>
2025-04-24 16:10:49 +0200

    restyling homepage
    prima parte

--- client/src/app/components/grid/grid.component.ts
+++ client/src/app/components/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { GridsterComponent, GridsterConfig, GridsterItemComponent } from 'angular-gridster2';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
@@ -17,7 +17,7 @@ import { DashboardItem } from '../../models/dashboard-item.model';
 })
 export class GridComponent {
   options!: GridsterConfig;
-  dashboard!: Array<DashboardItem>;
+  @Input() dashboard: Array<DashboardItem> = [];
 
   constructor(private router: Router) {}
 
@@ -36,7 +36,7 @@ export class GridComponent {
         enabled: true,
       },
       resizable: {
-        enabled: true,
+        enabled: false,
       },
       //griglia 12x12
       minCols: 12,
@@ -48,19 +48,6 @@ export class GridComponent {
       displayGrid: 'none',
       scrollToNewItems: true,
     };
-    //componenti della dashboard
-    this.dashboard = [
-      // Prima riga (y: 0)
-      { cols: 4, rows: 6, y: 0, x: 0, name: 'Calendario', relativeUrl: '/calendar', urlImg: "calendar.png", isTimeMachine: false },
-      { cols: 4, rows: 6, y: 0, x: 4, name: 'Timer', relativeUrl: '/timer', urlImg: "timer.png", isTimeMachine: false },
-      { cols: 4, rows: 6, y: 0, x: 8, name: 'Note', relativeUrl: '/note', urlImg: "note.png", isTimeMachine: false },
-    
-      // Seconda riga (y: 3)
-      { cols: 4, rows: 6, y: 3, x: 0, name: 'AsistenteAi', relativeUrl: '/assistant', urlImg: "chatbot.png", isTimeMachine: false },
-      { cols: 4, rows: 6, y: 3, x: 4, name: 'Extra 2', relativeUrl: '/extra2', urlImg: "extra.png", isTimeMachine: false },
-      { cols: 4, rows: 6, y: 3, x: 8, name: 'Time Machine', relativeUrl: '/time-machine', urlImg: "time-machine.png", isTimeMachine: true },
-    ];
-    
   }
   goToPage(url: string) {
     this.router.navigate([url]);
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { LogoutComponent } from "../logout/logout.component";
 import { GridComponent } from "../grid/grid.component";
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from '../../common/loading/loading.component';
+import { DashboardItem } from '../../models/dashboard-item.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css', '../../../assets/css/button.css'],
   imports: [
     LogoutComponent,
     GridComponent,
@@ -24,15 +25,30 @@ import { LoadingComponent } from '../../common/loading/loading.component';
 export class HomeComponent implements OnInit{ 
   user: User = new User();
   isLoading = true;
+  dashboard: Array<DashboardItem> = [
+    { cols: 0, rows: 0, y: 0, x: 0, name: 'Home', relativeUrl: '/home', urlImg: "home.png", isTimeMachine: false },
+    { cols: 4, rows: 6, y: 0, x: 0, name: 'Calendario', relativeUrl: '/calendar', urlImg: "calendar.png", isTimeMachine: false },
+    { cols: 4, rows: 6, y: 0, x: 4, name: 'Timer', relativeUrl: '/timer', urlImg: "timer.png", isTimeMachine: false },
+    { cols: 4, rows: 6, y: 0, x: 8, name: 'Note', relativeUrl: '/note', urlImg: "note.png", isTimeMachine: false },
+    { cols: 4, rows: 6, y: 3, x: 0, name: 'Assistente AI', relativeUrl: '/assistant', urlImg: "chatbot.png", isTimeMachine: false },
+    { cols: 4, rows: 6, y: 3, x: 4, name: 'Extra 2', relativeUrl: '/extra2', urlImg: "extra.png", isTimeMachine: false },
+    { cols: 4, rows: 6, y: 3, x: 8, name: 'Time Machine', relativeUrl: '/time-machine', urlImg: "time-machine.png", isTimeMachine: true },
+  ];
 
   constructor(private userService: UserService,
-              private authService: AuthService) {}
+              private authService: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {}
 
-    ngOnInit(): void {
-      this.userService.getUser(this.authService.currentUser).subscribe({
-        next: data => this.user = data,
-        error: error => console.log(error),
-        complete: () => this.isLoading = false
-      });
-    }
+  ngOnInit(): void {
+    this.userService.getUser(this.authService.currentUser).subscribe({
+      next: data => this.user = data,
+      error: error => console.log(error),
+      complete: () => this.isLoading = false
+    });
+  }
+
+  navigateTo(url: string) {
+    this.router.navigate([url], { relativeTo: this.route });
+  }
 }
