870dfe4329633ff0e6398738c581c8d982402d92
MatteMito <matteo.boscherini@studio.unibo.it>
2025-05-26 16:28:05 +0200

    inizio preview assistente ai

---
+++ client/src/app/preview-components/ai-assistant-preview/ai-assistant-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-assistant-preview',
+  imports: [],
+  templateUrl: './ai-assistant-preview.component.html',
+  styleUrl: './ai-assistant-preview.component.css'
+})
+export class AiAssistantPreviewComponent {
+
+}
